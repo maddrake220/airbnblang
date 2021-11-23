572a94ba87bdb51952d758561d3f4d8328b3258b
maddrake220 <jwchoi4118@gmail.com>
2021-11-24 01:50:49 +0900

    feat: can make airbnblang
    you can make aribnblang now. if you write korean sentence in textarea,
    and click the button, translated korean sentence will apear in output_textarea

--- src/airbnbnlang.js
+++ src/airbnbnlang.js
@@ -110,16 +110,116 @@ const testF = (el) => {
   }
 };
 
-function buttonHandler(e) {
-  let entireSentence = textarea_input.value.toString().trim().split("");
-  let str = getConstantVowel(entireSentence[0]);
-
-  if (str.t !== "") {
-    let random1 = Math.floor(Math.random() * 2);
-    let top = testF(str.f);
-    let middle = S.indexOf(str.s);
-    let bottom = T.indexOf(str.t);
-
-    console.log(String.fromCharCode(44032 + top * 588 + middle * 28 + bottom));
+const testS = (el) => {
+  let r = Math.floor(Math.random() * 2);
+  if (el === "ㅏ" || el === "ㅑ") {
+    return r === 0 ? 0 : 2;
+  } else if (el === "ㅐ" || el === "ㅒ") {
+    return r === 0 ? 1 : 3;
+  } else if (el === "ㅓ" || el === "ㅕ") {
+    return r === 0 ? 4 : 6;
+  } else if (el === "ㅔ" || el === "ㅖ") {
+    return r === 0 ? 5 : 7;
+  } else if (el === "ㅗ" || el === "ㅛ") {
+    return r === 0 ? 8 : 12;
+  } else if (el === "ㅙ" || el === "ㅚ") {
+    return r === 0 ? 10 : 11;
+  } else if (el === "ㅜ" || el === "ㅠ") {
+    return r === 0 ? 13 : 17;
+  } else if (el === "ㅢ" || el === "ㅡ") {
+    return r === 0 ? 18 : 19;
+  } else {
+    return S.indexOf(el);
   }
+};
+const testT = (el) => {
+  if (el === "ㄱ" || el === "ㄲ" || el === "ㄳ" || el === "ㄺ" || el === "ㅋ") {
+    let r = Math.floor(Math.random() * 5);
+    return r === 0 ? 1 : r === 1 ? 2 : r === 2 ? 3 : r === 3 ? 24 : 9;
+  } else if (el === "ㄴ" || el === "ㄵ" || el === "ㄶ") {
+    let r = Math.floor(Math.random() * 3);
+    return r === 0 ? 4 : r === 1 ? 5 : 6;
+  } else if (el === "ㄼ" || el === "ㄿ" || el === "ㅂ" || el === "ㅄ") {
+    let r = Math.floor(Math.random() * 4);
+    return r === 0 ? 11 : r === 1 ? 14 : r === 2 ? 17 : 18;
+  } else if (el === "ㅁ" || el === "ㄻ") {
+    let r = Math.floor(Math.random() * 2);
+    return r === 0 ? 16 : 10;
+  } else if (
+    el === "ㄷ" ||
+    el === "ㄽ" ||
+    el === "ㄾ" ||
+    el === "ㅅ" ||
+    el === "ㅆ" ||
+    el === "ㅈ" ||
+    el === "ㅊ" ||
+    el === "ㅌ" ||
+    el === "ㅎ" ||
+    el === "ㅀ"
+  ) {
+    let r = Math.floor(Math.random() * 9);
+    return r === 0
+      ? 7
+      : r === 1
+      ? 12
+      : r === 2
+      ? 13
+      : r === 3
+      ? 19
+      : r === 4
+      ? 20
+      : r === 5
+      ? 22
+      : r === 6
+      ? 23
+      : r === 7
+      ? 25
+      : r === 8
+      ? 15
+      : 27;
+  } else if (el === "") {
+    let r1 = Math.floor(Math.random() * 4);
+    let r;
+    if (r1 === 0) {
+      r = Math.floor(Math.random() * 27);
+    } else {
+      r = 0;
+    }
+    return r;
+  } else {
+    return T.indexOf(el);
+  }
+};
+function buttonHandler(e) {
+  const entireSentence = textarea_input.value.toString().trim().split("");
+  let answer = "";
+  for (let i in entireSentence) {
+    if (entireSentence[i] === " ") {
+      answer += " ";
+    } else if (entireSentence[i].charCodeAt(0) < 44032) {
+      answer += entireSentence[i];
+    } else {
+      let str = getConstantVowel(entireSentence[i]);
+      let top;
+      let middle;
+      let bottom;
+      let random = Math.floor(Math.random() * 2);
+      if (random === 0) {
+        top = testF(str.f);
+        middle = testS(str.s);
+        bottom = T.indexOf(str.t);
+      } else if (random === 1) {
+        top = testF(str.f);
+        middle = S.indexOf(str.s);
+        bottom = testT(str.t);
+      } else {
+        top = F.indexOf(str.f);
+        middle = testS(str.s);
+        bottom = testT(str.t);
+      }
+      answer += String.fromCharCode(44032 + top * 588 + middle * 28 + bottom);
+    }
+  }
+
+  textarea_output.innerText = answer;
 }
